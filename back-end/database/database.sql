234f0b06649fa5b41be7ef82c01e8d6c8e6ad0b5
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-03-28 18:45:21 +0300

    hosting commit

--- back-end/database/database.sql
+++ back-end/database/database.sql
@@ -5,43 +5,41 @@ CREATE TABLE taskTable(
   day VARCHAR,
   reminder BOOLEAN
 );
-SELECT
-  *
-FROM
-  taskTable;
+-- SELECT
+--   *
+-- FROM
+--   taskTable;
+-- INSERT INTO
+--   taskTable (text, day, reminder)
+-- VALUES($ 1, $ 2, $ 3) RETURNING *;
+-- SELECT
+--   *
+-- FROM
+--   taskTable
+-- WHERE
+--   id = $ 1;
+-- UPDATE
+--   taskTable
+-- SET
+--   text = $ 2 day = $ 3
+-- WHERE
+--   id = $ 1;
+-- DELETE FROM
+--   taskTable
+-- WHERE
+--   id = $ 1;
 INSERT INTO
   taskTable (text, day, reminder)
-VALUES($ 1, $ 2, $ 3) RETURNING *;
-SELECT
-  *
-FROM
-  taskTable
-WHERE
-  id = $ 1;
-UPDATE
-  taskTable
-SET
-  text = $ 2 day = $ 3
-WHERE
-  id = $ 1;
-DELETE FROM
-  taskTable
-WHERE
-  id = $ 1;
+VALUES('Code js', 'Saturday at 05:27 a.m', True);
 INSERT INTO
-  taskTable (text, day, reminder)
-VALUES('Code js', 'Saturday', True);
-INSERT INTO
-  tasktable (id, text, day, reminder)
+  tasktable (text, day, reminder)
 VALUES
   (
-    id :integer,
     'Design shop',
-    'Monday',
+    'Monday at 14:00H',
     True
   );
-
-ALTER TABLE tasktable RENAME COLUMN id TO id;
-ALTER TABLE tasktable RENAME COLUMN text TO text;
-ALTER TABLE tasktable RENAME COLUMN day TO day;
-ALTER TABLE tasktable RENAME COLUMN reminder TO reminder;
+-- ALTER TABLE tasktable RENAME COLUMN id TO id;
+  -- ALTER TABLE tasktable RENAME COLUMN text TO text;
+  -- ALTER TABLE tasktable RENAME COLUMN day TO day;
+  -- ALTER TABLE tasktable RENAME COLUMN reminder TO reminder;
--- back-end/database/db.js
+++ back-end/database/db.js
@@ -1,10 +1,10 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "postgres",
-  password: "@S3ns31;",
-  host: "0.0.0.0",
+  user: "zsvnjlabxzfxuz",
+  password: "31735e27702e8c04064aa29ed019dca39cba30d6d67f0f65174e166554786d3e",
+  host: "ec2-34-231-63-30.compute-1.amazonaws.com",
   port: 5432,
-  database: "tasksdb",
+  database: "deeodf263s05np",
 });
 module.exports = pool;
