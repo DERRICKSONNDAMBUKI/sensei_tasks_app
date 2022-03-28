45d374450794f2c4bb46a02448f885bd6b74a018
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-03-28 15:33:05 +0300

    get tasks and delete task commit

--- back-end/app.js
+++ back-end/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 app = express();
 const cors = require("cors");
+const pool = require("./database/db");
+const { param } = require("express/lib/request");
 
 // routes
 app.get("/", (req, res) => {
@@ -11,7 +13,28 @@ app.get("/", (req, res) => {
   }
 });
 // get all tasks
+app.get("/tasks", async (req, res) => {
+  try {
+    const allTasks = await pool.query("SELECT * FROM taskTable;");
+    res.json(allTasks.rows);
+    console.log("getting all tasks");
+  } catch (error) {
+    console.error(error);
+  }
+});
 
+// get one task
+app.delete("/tasks/:id", async (req, res) => {
+  try {
+      const {id} = req.params
+    const Tasks = await pool.query(
+        "DELETE FROM taskTable WHERE task_id = $1;",[id]);
+    res.json(`deleted task (${id}) successfully`);
+    console.log(`deleted task (${id}) successfully`);
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 
 const server = app.listen(8080, "0.0.0.0", () => {
