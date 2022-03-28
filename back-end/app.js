42c90032b87e08bba3da9ae68019c7a552f84019
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-03-28 13:23:06 +0300

    server commit

---
+++ back-end/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+app = express()
+const cors = require('cors');
+
+
+// routes
+app.get('/',(req,res)=>{
+    try {
+        res.send('<h1>Sensei Tasks Back-end</h1>')
+    } catch (error) {
+        console.error(error);
+    }
+})
+
+const server = app.listen(8080,'0.0.0.0',()=>{
+    const host = server.address().address
+    const port = server.address().port
+    console.log(`server is listening at http://${host}:${port}`);
+})
